b1a604dcd64c625f0572d9b448f28953f640df1d
wal <wal@kitmanlabs.com>
2017-03-13 10:03:45 +0000

    added variables

--- R/six_nations_2017.R
+++ R/six_nations_2017.R
@@ -1,8 +1,19 @@
 library(tidyverse)
 library(gridExtra)
 
-tournament_data_tibble = read.csv(file = 'data/tournament.csv')
+tournament_data = read.csv(file = 'data/tournament.csv')
+
+# Variables
 sort(names(tournament_data))
+
+# Tackles
+
+top_20_tackles_by_player <- tournament_data %>% 
+  group_by(player_name) %>% 
+  summarise(tackles = sum(tackles)) %>%
+  top_n(20, wt = tackles)
+
+top_20_tackles_by_player %>% order(tackles)
 
 # Turnovers
 
@@ -16,6 +27,7 @@ turnover_conceeded_plot <- ggplot(data = tournament_data) +
 
 grid.arrange(turnover_won_plot, turnover_conceeded_plot)
 
+
 # Total Lineouts
 
 ggplot(data = tournament_data) + 
@@ -26,11 +38,9 @@ ggplot(data = tournament_data) +
 offloads_plot <- ggplot(data = tournament_data) +
   geom_bar(mapping = aes(y = offload, x = team), stat = 'identity')
 
-grid.arrange(passes_plot, 
-             offloads_plot)
+grid.arrange(passes_plot, offloads_plot)
 
 res <- cor(tournament_data)
-
 
 ireland_data <- tournament_data %>% filter(team == 'Ireland')
 
@@ -38,3 +48,4 @@ ggplot(data = ireland_data) +
   geom_boxplot(mapping = aes(x = player_name, y = carries_metres)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
   facet_grid(~ opposition)
+
